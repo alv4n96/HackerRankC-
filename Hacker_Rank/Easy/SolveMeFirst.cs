e880841267f277ced34281e3341f63368560327f
alvan <alvan.ghon@gmail.com>
2022-12-30 23:18:34 +0700

    add file test SolveMeFirst

---
+++ Hacker_Rank/Easy/SolveMeFirst.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker_Rank.Easy
+{
+    internal class SolveMeFirst
+    {
+        public static void Run()
+        {
+            //-----------------hackerrank
+            int d = 18, m = 7;
+
+
+            int res = solveMeFirstTest(d, m);
+            Console.WriteLine(res);
+
+        }
+
+        private static int solveMeFirstTest(int a, int b)
+        {
+            // Hint: Type return a+b; below  
+            return a + b;
+
+        }
+    }
+}
