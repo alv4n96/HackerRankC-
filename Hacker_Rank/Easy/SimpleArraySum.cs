08281139778dcffe3fc1fac96b9eb33de3e90196
alvan <alvan.ghon@gmail.com>
2022-12-30 23:23:50 +0700

    add file test SimpleArraySum

---
+++ Hacker_Rank/Easy/SimpleArraySum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hacker_Rank.Easy
+{
+    internal class SimpleArraySum
+    {
+        public static void Run()
+        {
+            //-----------------hackerrank
+            List<int> ar = new List<int>() { 1, 2, 3, 4, 10, 11 };
+
+
+            int res = simpleArraySumTest(ar);
+            Console.WriteLine(res);
+
+        }
+
+        public static int simpleArraySumTest(List<int> ar)
+        {
+            int total = 0;
+            foreach (int a in ar) total += a;
+            return total;
+        }
+    }
+}
